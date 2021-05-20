cc674bf4aa3486421f44cc541afbe33a28d5dd01
kenshin1024 <imanishi555@icloud.com>
2021-05-20 21:27:35 +0900

    バリデーション記述の編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-  validates :nickname, presence: true  
+   
   validates :password,  format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
-  validates :last_name,  presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
-  validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー]+\z/}
-  validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー]+\z/}
-  validates :birthday, presence: true
 
+  with_options presence: true do
+  validates :nickname
+  validates :birthday
+  with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
+  validates :last_name
+  validates :first_name
+  end
+  with_options format: {with: /\A[ァ-ヶー]+\z/} do
+  validates :last_name_kana
+  validates :first_name_kana
+ end
+end
 end
