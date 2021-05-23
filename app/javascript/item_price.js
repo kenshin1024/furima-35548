a33aab770727bef2eb42cdd0265cd5ae9b7ddf13
kenshin1024 <imanishi555@icloud.com>
2021-05-23 11:15:25 +0900

    コメントアウトとデバックの削除

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,5 +1,4 @@
 window.addEventListener('load', () =>{
-  console.log("OK");
   const itemPrice = document.getElementById("item-price")
   const addTaxDom = document.getElementById("add-tax-price")
   const profitDom = document.getElementById("profit");
