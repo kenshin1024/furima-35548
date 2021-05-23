2ea54b88bf6298b6871dc771f96b88201743f913
kenshin1024 <imanishi555@icloud.com>
2021-05-23 19:23:15 +0900

    factoryのファイルにアソシエーションを定義

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :item do
+    association         :user
     title               {"a"}
     description         {"a"}
     category_id         {2}
@@ -8,7 +9,6 @@ FactoryBot.define do
     prefecture_id       {2}
     scheduled_delivery_id  {2}
     price               {500}
-    user_id             {1}
 
     after(:build) do |item|
       item.image.attach(io: File.open('public/images/sample.png'), filename: 'sample.png')
