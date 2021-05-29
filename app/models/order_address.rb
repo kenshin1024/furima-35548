4c37be6d774b11495cbe8385c6277f4fdd07b775
kenshin1024 <imanishi555@icloud.com>
2021-05-29 13:36:45 +0900

    formオブジェクトの作成

---
+++ app/models/order_address.rb
@@ -0,0 +1,24 @@
+def OrderAddress
+  include ActiveModel::Model
+  attr_acccessor :order_id, :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :user_id, :item_id
+
+  with_options presence: true do
+    validates :postal_code
+    validates :prefecture_id
+    validates :city
+    validates :address
+    validates :building
+    validates :phone_number
+    with_options foreign_key: true do
+      validates :order_id 
+      validates :user_id
+      validates :item_id
+    end
+  end
+ 
+  def save
+    order = Order.create(user: user, item: item)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_number: phone_number, order_id: order.id)
+  end
+  
+end
